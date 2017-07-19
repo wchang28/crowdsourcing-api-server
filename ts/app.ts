38f91b5518a8b0f6d86becc69638b4ee0c5beca7
wchang28 <wchang28@hotmail.com>
2017-07-19 16:47:19 -0400

    remove app-factory.ts

--- ts/app.ts
+++ ts/app.ts
@@ -1,24 +1,80 @@
 import * as express from 'express';
 import {IWebServerConfig, startServer} from 'express-web-server';
-import * as af from "./app-factory";
+import * as bodyParser from "body-parser";
+import noCache = require('no-cache-express');
+import * as prettyPrinter from 'express-pretty-print';
+import {getAllExtensionModules} from "./extensions";
+import {ExtensionModuleExport, AppGlobal} from "crowdsourcing-api";
+import * as rcf from "rcf";
+import * as node$ from "rest-node";
 
 let NODE_PATH = process.env["NODE_PATH"];
-let Port = (process.argv.length >= 3 ? parseInt(process.argv[2]) : 80);
+let Port = (process.env["PORT"] ? parseInt(process.env["PORT"]) : 80);
 
 console.log("NODE_PATH=" + NODE_PATH);
-console.log("Port=" + Port);
+console.log("Port=" + Port.toString());
 
-function startApiAppServer(appFactory: af.IAPIAppFactory, port: number, callback?: () => void) {
-    console.log(new Date().toISOString() + ": starting the crowdsourcing api server...");
-    startServer({http:{port, host: "127.0.0.1"}}, appFactory.create(), (secure:boolean, host:string, port:number) => {
-        let protocol = (secure ? 'https' : 'http');
-        console.log(new Date().toISOString() + ': crowdsourcing api server listening at %s://%s:%s', protocol, host, port);
-        if (typeof callback === "function") callback();
-    }, (err:any) => {
-        console.error(new Date().toISOString() + ': !!! crowdsourcing api server error: ' + JSON.stringify(err));
-        process.exit(1);
-    });
+if (!NODE_PATH) {
+    console.error("env['NODE_PATH'] is not set");
+    process.exit(1);
 }
 
-let appFactory = af.get({SelfPort: Port});
-startApiAppServer(appFactory, Port);
+let app = express();
+
+app.set('jsonp callback name', 'cb');
+
+app.use(noCache);
+app.use(bodyParser.text({"limit":"999mb"}));
+app.use(bodyParser.json({"limit":"999mb"}));
+app.use(prettyPrinter.get());
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.options("/*", (req: express.Request, res: express.Response) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH,HEAD');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Content-Length,X-Requested-With');
+    res.send(200);
+});
+
+let selfApi = new rcf.AuthorizedRestApi(node$.get(), {instance_url: "http://127.0.0.1:" + Port.toString()});
+let selfApiRoute = selfApi.mount("/");
+let g: AppGlobal = {
+    selfApiRoute
+};
+
+app.set("global", g);   // set the global object
+
+let serviceRouter = express.Router();
+
+let extensionModules = getAllExtensionModules(NODE_PATH);
+for (let i in extensionModules) {   // for each module
+    let module = extensionModules[i].module;
+    try {
+        let moduleExport: ExtensionModuleExport = require(module);
+        let moduleRouter = express.Router();
+        serviceRouter.use("/" + module, moduleRouter);
+        moduleExport.init(moduleRouter);
+    } catch(e) {
+
+    }
+}
+
+app.use("/services", serviceRouter);
+
+app.get("/doc/api-listing", (req: express.Request, res: express.Response) => {
+    res.jsonp(extensionModules);
+})
+
+console.log(new Date().toISOString() + ": starting the crowdsourcing api server...");
+
+startServer({http:{port: Port, host: "127.0.0.1"}}, app, (secure:boolean, host:string, port:number) => {
+    let protocol = (secure ? 'https' : 'http');
+    console.log(new Date().toISOString() + ': crowdsourcing api server listening at %s://%s:%s', protocol, host, port);
+}, (err:any) => {
+    console.error(new Date().toISOString() + ': !!! crowdsourcing api server error: ' + JSON.stringify(err));
+    process.exit(1);
+});
