fb60dff70afa1f624aaf4d3729141da69bae07fa
wchang28 <wchang28@hotmail.com>
2017-07-24 15:21:32 -0400

    lgkhkdnhghd

--- ts/extensions.ts
+++ ts/extensions.ts
@@ -1,12 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
-export interface ExtensionModule {
-	module: string;
-	homePath: string;
-    package_json: any;
-    README_md?: string;
-}
+import {ExtensionModule} from "crowdsourcing-api";
 
 export function getAllExtensionModules(NODE_PATH: string) : ExtensionModule[] {
     let ret: ExtensionModule[] = [];
